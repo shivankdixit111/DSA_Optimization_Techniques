14bb86475bbde95b4c3fcbcfb5994a407a79b989
Shivank Dixit <137625408+shivankdixit111@users.noreply.github.com>
2026-06-12 11:29:23 +0530

    Optimize Binary Lifting with dynamic parent size

--- BinaryLifting/code.cpp
+++ BinaryLifting/code.cpp
@@ -2,10 +2,12 @@ class BinaryLifting {
     public:
       vector<int> height, parent;
       vector<vector<int>> kthParent;
+      int m;
       
     BinaryLifting(int n, vector<vector<int>> adj) {
+        this->m = log2(n);
         height.resize(n+1,0), parent.resize(n+1,-1);
-        kthParent.resize(n+1, vector<int>(31,-1));
+        kthParent.resize(n+1, vector<int>(m+1,-1));
 
         auto dfs = [&](int node, int par, int h, auto &&self)->void{
            height[node] = h;
@@ -19,7 +21,7 @@ class BinaryLifting {
         dfs(1,-1,0,dfs);
 
         for(int i=1; i<=n; i++) kthParent[i][0] = parent[i];
-        for(int j=1; j<31; j++) {
+        for(int j=1; j<=m; j++) {
             for(int i=1; i<=n; i++) {
                 int par = kthParent[i][j-1];
                 if(par != -1) kthParent[i][j] = kthParent[par][j-1];
@@ -29,7 +31,7 @@ class BinaryLifting {
 
     int findKthParent(int node, int k) {
         int at = node;
-        for(int i=0; i<31; i++) {
+        for(int i=0; i<=m; i++) {
             if((1<<i) & k) {
                 at = kthParent[at][i];
             }
@@ -43,7 +45,7 @@ class BinaryLifting {
         v = findKthParent(v, diff); //uplift the v
         if(u==v) return u;
 
-        for(int i=30; i>=0; i--) {
+        for(int i=m; i>=0; i--) {
             int new_u = kthParent[u][i];
             int new_v = kthParent[v][i];
             if(new_u == new_v) continue;
